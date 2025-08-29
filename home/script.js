d6d1289f5b0a08a18bde724f3973f3ef221d6733
Abrham Kifle Kebede <abrihott@gmail.com>
2025-08-29 23:42:34 +0300

    Update script.js

--- home/script.js
+++ home/script.js
@@ -1,6 +1,4 @@
-const wrapper = document.querySelector('.wrapper')
-const registerLink = document.querySelector('.register-link')
-const loginLink = document.querySelector('.login-link')
+
 
 registerLink.onclick = () => {
     wrapper.classList.add('active')
